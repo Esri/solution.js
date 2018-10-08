8f08e3e267f204c952ac9813128fa1142391afd2
Mike Tschudi <mtschudi@esri.com>
2018-10-08 15:36:34 -0700

    Added config files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # arcgis-clone-js
 Transfer sets of ArcGIS Online items from one organization to another
+
+* cd to repository directory
+* npm init -y
+* open file and change author to "Esri" and license from "ISC" to "Apache-2.0".
+
+
+
+
---
+++ set_up_node.bat
@@ -0,0 +1,27 @@
+call npm init -y
+
+call npm install --save-dev @esri/arcgis-rest-auth
+call npm install --save-dev @esri/arcgis-rest-common-types
+call npm install --save-dev @esri/arcgis-rest-feature-service-admin
+call npm install --save-dev @esri/arcgis-rest-groups
+call npm install --save-dev @esri/arcgis-rest-items
+call npm install --save-dev @esri/arcgis-rest-request
+call npm install --save-dev @esri/arcgis-rest-sharing
+call npm install --save-dev @types/arcgis-js-api
+call npm install --save-dev @types/fetch-mock
+call npm install --save-dev @types/isomorphic-fetch
+call npm install --save-dev @types/jasmine
+call npm install --save-dev @types/node
+call npm install --save-dev fetch-mock
+call npm install --save-dev jasmine
+call npm install --save-dev jasmine-core
+call npm install --save-dev node-fetch
+call npm install --save-dev ts-node
+call npm install --save-dev typescript
+
+call npm install --save-prod es6-promise
+call npm install --save-prod isomorphic-fetch
+call npm install --save-prod isomorphic-form-data
+call npm install --save-prod tslib
+
+call npm list -depth=0
