4069040740a827444d79dea8866a0302eb9cfbba
Mike Tschudi <mtschudi@esri.com>
2018-10-10 14:13:13 -0700

    Cleaned up item type files, stage 1

--- types/item.d.ts
+++
@@ -1,9 +0,0 @@
-import { IRequestOptions } from "@esri/arcgis-rest-request";
-import { AgolItem } from "./agolItem";
-export declare class Item extends AgolItem {
-    /**
-     * Item data section JSON
-     */
-    dataSection?: any;
-    init(requestOptions?: IRequestOptions): Promise<AgolItem>;
-}
