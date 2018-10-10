8fe73cba5df8cd1a88b4c78de3492f6fa4212cc1
Mike Tschudi <mtschudi@esri.com>
2018-10-10 14:42:46 -0700

    Restored types

---
+++ types/test/mocks/dashboard.d.ts
@@ -0,0 +1,3 @@
+import { IItem } from "@esri/arcgis-rest-common-types";
+export declare const DashboardItemSuccessResponse: IItem;
+export declare const DashboardItemDataSuccessResponse: any;
---
+++ types/test/mocks/webmap.d.ts
@@ -0,0 +1,3 @@
+import { IItem } from "@esri/arcgis-rest-common-types";
+export declare const WebMapItemSuccessResponse: IItem;
+export declare const WebMapItemDataSuccessResponse: any;
