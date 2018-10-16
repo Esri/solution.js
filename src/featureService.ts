fd3251f8295eb9687b1863003e5389ed633fc402
Mike Tschudi <mtschudi@esri.com>
2018-10-16 10:35:08 -0700

    Fixed copyright statements

--- src/featureService.ts
+++ src/featureService.ts
@@ -1,5 +1,18 @@
-/* Copyright (c) 2018 Esri
- * Apache-2.0 */
+/*
+ | Copyright 2018 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
 
 import { IRequestOptions, request } from "@esri/arcgis-rest-request";
 import { AgolItem } from "./agolItem";
@@ -41,7 +54,7 @@ export class FeatureService extends Item {
           // full tables
 
           // Add to the standard cost of creating an item: extra for creating because they're
-          // slower than other items to create and extra because they have to be moved to the 
+          // slower than other items to create and extra because they have to be moved to the
           // desired folder
           this.estimatedCost += 2;
 
