4f3d103d3ee00498e82f5a040dce81843d9d9131
Mike Tschudi <mtschudi@esri.com>
2019-01-31 13:25:55 -0800

    Removed some webpack entrypoints

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,6 @@
 const path = require('path');
 const destination = path.resolve(__dirname, 'dist');
 
-/*
-module.exports = {
-  entry: './dist/src/index.js',
-  mode: 'production',
-  output: {
-    library: 'arcgis_clone_js',
-    filename: 'arcgis_clone_js.js',
-    path: destination
-  }
-};
-*/
-
 module.exports = [{
   entry: './dist/umd/arcgis-clone.umd.js',
   mode: 'production',
@@ -22,35 +10,11 @@ module.exports = [{
     path: destination
   }
 }, {
-  entry: '../adlib/dist/adlib.js',
-  mode: 'production',
-  output: {
-    library: 'adlib',
-    filename: 'adlib.js',
-    path: destination
-  }
-}, {
   entry: '../arcgis-rest-js/packages/arcgis-rest-auth/dist/umd/auth.umd.min.js',
   mode: 'production',
   output: {
     library: 'arcgis_rest_auth',
     filename: 'arcgis-rest-auth.js',
-    path: destination
-  }
-}, {
-  entry: '../arcgis-rest-js/packages/arcgis-rest-feature-service-admin/dist/umd/feature-service-admin.umd.min.js',
-  mode: 'production',
-  output: {
-    library: 'arcgis_rest_feature_service_admin',
-    filename: 'arcgis-rest-feature-service-admin.js',
-    path: destination
-  }
-}, {
-  entry: '../arcgis-rest-js/packages/arcgis-rest-groups/dist/umd/groups.umd.min.js',
-  mode: 'production',
-  output: {
-    library: 'arcgis_rest_groups',
-    filename: 'arcgis-rest-groups.js',
     path: destination
   }
 }, {
@@ -67,14 +31,6 @@ module.exports = [{
   output: {
     library: 'arcgis_rest_request',
     filename: 'arcgis-rest-request.js',
-    path: destination
-  }
-}, {
-  entry: '../arcgis-rest-js/packages/arcgis-rest-sharing/dist/umd/sharing.umd.min.js',
-  mode: 'production',
-  output: {
-    library: 'arcgis_rest_sharing',
-    filename: 'arcgis-rest-sharing.js',
     path: destination
   }
 }];
