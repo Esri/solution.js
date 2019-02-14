887c3873eae8406573d2b1b55dea00c5ddbe56b8
Mike Tschudi <mtschudi@esri.com>
2019-02-13 21:16:56 -0800

    Switched to CDN for arcgis-rest-js

--- webpack.config.js
+++ webpack.config.js
@@ -9,35 +9,4 @@ module.exports = [{
     filename: 'arcgis-clone-js.js',
     path: destination
   }
-}, {
-  entry: '../adlib/dist/adlib.js',
-  mode: 'production',
-  output: {
-    library: 'adlib',
-    filename: 'adlib.js',
-    path: destination
-  }}, {
-  entry: '../arcgis-rest-js/packages/arcgis-rest-auth/dist/umd/auth.umd.min.js',
-  mode: 'production',
-  output: {
-    library: 'arcgis_rest_auth',
-    filename: 'arcgis-rest-auth.js',
-    path: destination
-  }
-}, {
-  entry: '../arcgis-rest-js/packages/arcgis-rest-items/dist/umd/items.umd.min.js',
-  mode: 'production',
-  output: {
-    library: 'arcgis_rest_items',
-    filename: 'arcgis-rest-items.js',
-    path: destination
-  }
-}, {
-  entry: '../arcgis-rest-js/packages/arcgis-rest-request/dist/umd/request.umd.min.js',
-  mode: 'production',
-  output: {
-    library: 'arcgis_rest_request',
-    filename: 'arcgis-rest-request.js',
-    path: destination
-  }
 }];
