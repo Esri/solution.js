e87431ce4e7f37a100edb5ce6759fbfd8b2cb376
Mike Tschudi <mtschudi@esri.com>
2019-05-20 11:55:20 -0700

    Reverted Karma exclusion of .d.ts files

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
     files: ["packages/*/{src,test}/**/*.ts"],
 
     // list of files to exclude
-    exclude: [],
+    exclude: ["packages/*/{src,test}/**/*.d.ts"],
 
     karmaTypescriptConfig: {
       coverageOptions: {
