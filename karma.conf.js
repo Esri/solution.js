4c0ff181bd7078edde2f838c01dd617fdc245590
Mike Tschudi <mtschudi@esri.com>
2019-05-20 09:28:13 -0700

    Temporarily remove coverage testing

--- karma.conf.js
+++ karma.conf.js
@@ -68,7 +68,8 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["spec", "karma-typescript"],
+    // reporters: ["spec", "karma-typescript"],
+    reporters: ["spec"],
 
     // web server port
     port: 9876,
