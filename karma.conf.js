92b6247546b0367bdc295c54b9a8c7e94de6be8b
Mike Tschudi <mtschudi@esri.com>
2019-05-23 08:49:20 -0700

    Restored `coverage` reporter

--- karma.conf.js
+++ karma.conf.js
@@ -68,8 +68,8 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    // reporters: ["spec", "karma-typescript"],
-    reporters: ["spec"],
+    // reporters: ["spec", "karma-typescript", "coverage"],
+    reporters: ["spec", "coverage"],
 
     // web server port
     port: 9876,
