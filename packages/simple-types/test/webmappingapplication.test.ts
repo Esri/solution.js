84f7a166b16d90165f9aebcecab13a7046e3bff0
John Hauck <jhauck@esri.com>
2019-10-16 11:22:36 -0600

    handle web app templates values collection

--- packages/simple-types/test/webmappingapplication.test.ts
+++ packages/simple-types/test/webmappingapplication.test.ts
@@ -105,6 +105,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           e => done.fail(e)
         );
     });
+
     it("just group data", done => {
       const model = {
         itemId: "itm1234567890",
@@ -138,6 +139,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           e => done.fail(e)
         );
     });
+
     it("neither webmap nor group", done => {
       const model = {
         itemId: "itm1234567890",
@@ -187,6 +189,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           }
         );
     });
+
     it("webmap data with external dataSources", done => {
       const model = {
         itemId: "itm1234567890",
@@ -282,6 +285,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           e => done.fail(e)
         );
     });
+
     it("error with webmap data with external dataSources", done => {
       const model = {
         itemId: "itm1234567890",
@@ -335,6 +339,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           e => done()
         );
     });
+
     it("webmap data with external dataSources without url", done => {
       const model = {
         itemId: "itm1234567890",
@@ -415,6 +420,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           e => done.fail(e)
         );
     });
+
     it("webmap data with widgetPool widgets", done => {
       const model = {
         itemId: "f3223bda3c304dd0bf46dee75ac31aae",
@@ -550,6 +556,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           e => done.fail(e)
         );
     });
+
     it("webmap data with widgetPool and widgetOnScreen widgets", done => {
       const model = {
         itemId: "f3223bda3c304dd0bf46dee75ac31aae",
@@ -723,6 +730,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           e => done.fail(e)
         );
     });
+
     it("error with widgetPool widgets", done => {
       const model = {
         itemId: "f3223bda3c304dd0bf46dee75ac31aae",
@@ -797,6 +805,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           e => done()
         );
     });
+
     it("error with widgetOnScreen widgets", done => {
       const model = {
         itemId: "f3223bda3c304dd0bf46dee75ac31aae",
@@ -870,6 +879,55 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
           },
           e => done()
         );
+    });
+  });
+
+  describe("templatizeDatasources ", () => {
+    xit("templatizeDatasources ", done => {
+      console.warn("========== TODO ==========");
+      done.fail();
+    });
+  });
+
+  describe("templatizeWidgets ", () => {
+    xit("templatizeWidgets ", done => {
+      console.warn("========== TODO ==========");
+      done.fail();
+    });
+  });
+
+  describe("templatizeValues ", () => {
+    xit("templatizeValues ", done => {
+      console.warn("========== TODO ==========");
+      done.fail();
+    });
+  });
+
+  describe("handleServiceRequests ", () => {
+    xit("handleServiceRequests ", done => {
+      console.warn("========== TODO ==========");
+      done.fail();
+    });
+  });
+
+  describe("findUrls ", () => {
+    xit("findUrls ", done => {
+      console.warn("========== TODO ==========");
+      done.fail();
+    });
+  });
+
+  describe("replaceUrl ", () => {
+    xit("replaceUrl ", done => {
+      console.warn("========== TODO ==========");
+      done.fail();
+    });
+  });
+
+  describe("setValues ", () => {
+    xit("setValues ", done => {
+      console.warn("========== TODO ==========");
+      done.fail();
     });
   });
 
@@ -1054,6 +1112,13 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
       const expected = ["abc"];
       const actual = webmappingapplication._getWABDependencies(model);
       expect(actual).toEqual(expected);
+    });
+  });
+
+  describe("_templatizeIdPaths ", () => {
+    xit("_templatizeIdPaths ", done => {
+      console.warn("========== TODO ==========");
+      done.fail();
     });
   });
 
@@ -1325,15 +1390,15 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
     });
   });
 
-  describe("_templatizeDatasourceFieldReferences", () => {
-    xit("_templatizeDatasourceFieldReferences", done => {
+  describe("_templatizeObject", () => {
+    xit("_templatizeObject", done => {
       console.warn("========== TODO ==========");
       done.fail();
     });
   });
 
-  describe("_templatizeWidgetFieldReferences", () => {
-    xit("_templatizeWidgetFieldReferences", done => {
+  describe("_templatizeObjectArray", () => {
+    xit("_templatizeObjectArray", done => {
       console.warn("========== TODO ==========");
       done.fail();
     });
@@ -1374,3 +1439,245 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
     });
   });
 });
+
+const expectedInfoLookupValues: any = {
+  values: {
+    serviceAreaLayerNames: "Service Area",
+    serviceAreaLayerNamesSelector:
+      '[{"id":"TestLayer2FromWebApp_4042","fields":["{{b19aec399444407da84fffe2a55d4151.layer3.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayer2FromWebApp_8439","fields":["{{b19aec399444407da84fffe2a55d4151.layer2.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayer2FromWebApp_5607","fields":["{{b19aec399444407da84fffe2a55d4151.layer1.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayer2FromWebApp_9409","fields":["{{b19aec399444407da84fffe2a55d4151.layer0.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayerForDashBoardMap_2615","fields":["{{4efe5f693de34620934787ead6693f19.layer7.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayerForDashBoardMap_8627","fields":["{{4efe5f693de34620934787ead6693f19.layer6.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayerForDashBoardMap_7797","fields":["{{4efe5f693de34620934787ead6693f19.layer5.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayerForDashBoardMap_5389","fields":["{{4efe5f693de34620934787ead6693f19.layer4.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayerForDashBoardMap_5538","fields":["{{4efe5f693de34620934787ead6693f19.layer3.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayerForDashBoardMap_2914","fields":["{{4efe5f693de34620934787ead6693f19.layer2.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayerForDashBoardMap_7041","fields":["{{4efe5f693de34620934787ead6693f19.layer1.fields.objectid.name}}"],"type":"FeatureLayer"},{"id":"TestLayerForDashBoardMap_2892","fields":["{{4efe5f693de34620934787ead6693f19.layer0.fields.objectid.name}}"],"type":"FeatureLayer"}]',
+    popupTitle: "Service Information",
+    popupWidth: null,
+    popupHeight: null,
+    serviceUnavailableTitle: "Outside Service Area",
+    serviceUnavailableMessage:
+      "No information is available at the selected location.",
+    noSearchFeatureTitle: "No Search Feature Found",
+    noSearchFeatureMessage:
+      "A search feature used to lookup information was not found.  Please select a new location.",
+    zoomLevel: 18,
+    storeLocation: true,
+    serviceRequestLayerAvailibiltyFieldValueAvail: "Intersected",
+    serviceRequestLayerAvailibiltyFieldValueNotAvail: "Not Intersected",
+    serviceRequestLayerAvailibiltyFieldValueNoSearch: "No Search Feature",
+    showSplash: false,
+    splashText:
+      "<center>Information Lookup is a configurable web application template that can be used to provide the general public, internal staff and other interested parties the with information about a location. If no features are found at that location, a general message is displayed. Optionally, the location entered can be stored in a point layer. The template can be configured using the ArcGIS Online Configuration dialog.</center>",
+    basemapWidgetVisible: true,
+    search: true,
+    title: "Information Lookup",
+    color: "#FFFFFF",
+    backcolor: "#000000",
+    hypercolor: "#0000EE",
+    uidirection: "left",
+    splashHeight: 350,
+    splashWidth: 290,
+    showUI: false,
+    popupSide: false,
+    popPostMessage: "",
+    popPreMessage: "",
+    orientForMobile: false,
+    linksInPopup: false,
+    linksInPopupSide: true,
+    minLineSize: 1,
+    minPolygonSize: 5,
+    checkSize: false,
+    onlySearchFeature: false,
+    searchTol: 4,
+    pointOverlap: 40,
+    pointOverlapUnit: "feet",
+    searchConfig: {
+      sources: [
+        {
+          locator: {
+            url: "{{organization.geocodeServerUrl}}",
+            _url: {
+              path: "{{organization.geocodeServerUrl}}",
+              query: null
+            },
+            normalization: true
+          },
+          url: "{{organization.geocodeServerUrl}}",
+          northLat: "Ymax",
+          southLat: "Ymin",
+          eastLon: "Xmax",
+          westLon: "Xmin",
+          name: "ArcGIS World Geocoding Service",
+          placefinding: true,
+          batch: true,
+          enableSuggestions: true,
+          singleLineFieldName: "SingleLine",
+          enable: true,
+          id: "dojoUnique393"
+        },
+        {
+          flayerId: "TestLayer2FromWebApp_4042",
+          url: "{{b19aec399444407da84fffe2a55d4151.layer3.url}}",
+          name: "TestLayer2FromWebApp - Stands",
+          id: "dojoUnique394",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{b19aec399444407da84fffe2a55d4151.layer3.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayer2FromWebApp_8439",
+          url: "{{b19aec399444407da84fffe2a55d4151.layer2.url}}",
+          name: "TestLayer2FromWebApp - Property",
+          id: "dojoUnique395",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{b19aec399444407da84fffe2a55d4151.layer2.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayer2FromWebApp_5607",
+          url: "{{b19aec399444407da84fffe2a55d4151.layer1.url}}",
+          name: "TestLayer2FromWebApp - Chemical Activity",
+          id: "dojoUnique396",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{b19aec399444407da84fffe2a55d4151.layer1.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayer2FromWebApp_9409",
+          url: "{{b19aec399444407da84fffe2a55d4151.layer0.url}}",
+          name: "TestLayer2FromWebApp - HarvestActivity",
+          id: "dojoUnique397",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{b19aec399444407da84fffe2a55d4151.layer0.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayerForDashBoardMap_2615",
+          url: "{{4efe5f693de34620934787ead6693f19.layer7.url}}",
+          name: "TestLayerForDashBoardMap - Incident Area",
+          id: "dojoUnique398",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{4efe5f693de34620934787ead6693f19.layer7.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayerForDashBoardMap_8627",
+          url: "{{4efe5f693de34620934787ead6693f19.layer6.url}}",
+          name: "TestLayerForDashBoardMap - DemographicPolygons",
+          id: "dojoUnique399",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{4efe5f693de34620934787ead6693f19.layer6.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayerForDashBoardMap_7797",
+          url: "{{4efe5f693de34620934787ead6693f19.layer5.url}}",
+          name: "TestLayerForDashBoardMap - Road Closure",
+          id: "dojoUnique400",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{4efe5f693de34620934787ead6693f19.layer5.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayerForDashBoardMap_5389",
+          url: "{{4efe5f693de34620934787ead6693f19.layer4.url}}",
+          name: "TestLayerForDashBoardMap - Bridges",
+          id: "dojoUnique401",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{4efe5f693de34620934787ead6693f19.layer4.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayerForDashBoardMap_5538",
+          url: "{{4efe5f693de34620934787ead6693f19.layer3.url}}",
+          name: "TestLayerForDashBoardMap - Emergency Assistance",
+          id: "dojoUnique402",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{4efe5f693de34620934787ead6693f19.layer3.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayerForDashBoardMap_2914",
+          url: "{{4efe5f693de34620934787ead6693f19.layer2.url}}",
+          name: "TestLayerForDashBoardMap - Emergency Shelter",
+          id: "dojoUnique403",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{4efe5f693de34620934787ead6693f19.layer2.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayerForDashBoardMap_7041",
+          url: "{{4efe5f693de34620934787ead6693f19.layer1.url}}",
+          name: "TestLayerForDashBoardMap - School",
+          id: "dojoUnique404",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{4efe5f693de34620934787ead6693f19.layer1.fields.objectid.name}}"
+          ]
+        },
+        {
+          flayerId: "TestLayerForDashBoardMap_2892",
+          url: "{{4efe5f693de34620934787ead6693f19.layer0.url}}",
+          name: "TestLayerForDashBoardMap - Hospital",
+          id: "dojoUnique405",
+          enable: true,
+          placeholder: "",
+          searchFields: [
+            "{{4efe5f693de34620934787ead6693f19.layer0.fields.objectid.name}}"
+          ]
+        }
+      ],
+      activeSourceIndex: "all",
+      enableSearchingAll: true
+    },
+    webmap: "{{eb6dc49be6f44f76aa195d6de8ce5c48.itemId}}",
+    serviceRequestLayerName: {
+      id: "TestLayerForDashBoardMap_5538",
+      fields: [
+        {
+          id: "serviceRequestLayerAvailibiltyField",
+          fields: [
+            "{{4efe5f693de34620934787ead6693f19.layer3.fields.contactid.name}}"
+          ]
+        }
+      ]
+    },
+    searchByLayer: {
+      id: "TestLayerForDashBoardMap_7797",
+      fields: [
+        {
+          id: "urlField",
+          fields: [
+            "{{4efe5f693de34620934787ead6693f19.layer5.fields.objectid.name}}"
+          ]
+        }
+      ]
+    },
+    customUrlLayer: {
+      id: "TestLayerForDashBoardMap_5389",
+      fields: [
+        {
+          id: "urlField",
+          fields: [
+            "{{4efe5f693de34620934787ead6693f19.layer4.fields.objectid.name}}"
+          ]
+        }
+      ]
+    }
+  }
+};
