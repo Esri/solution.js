34797f1d5d328bbe7705e9294d13d8c7fc0ddaac
Mike Tschudi <mtschudi@esri.com>
2020-01-31 15:49:41 -0800

    Added tests

--- packages/simple-types/test/webmappingapplication.test.ts
+++ packages/simple-types/test/webmappingapplication.test.ts
@@ -1242,7 +1242,7 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
   });
 
   describe("templatizeWidgets ", () => {
-    it("handles widgets that are missing properties", done => {
+    it("handles widgets", done => {
       const itemTemplate: common.IItemTemplate = {
         itemId: "f3223bda3c304dd0bf46dee75ac31aae",
         type: "Web Mapping Application",
@@ -1293,6 +1293,15 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
                     service: "http://fake.maps.arcgis.com/NAServer"
                   }
                 }
+              },
+              {
+                icon: "https://somepath/anothername.png",
+                config: {
+                  someProperty: {
+                    someHttpUrl:
+                      "http://fake.com/arcgis/rest/services/test/FeatureServer/2"
+                  }
+                }
               }
             ]
           }
@@ -1327,8 +1336,109 @@ describe("Module `webmappingapplication`: manages the creation and deployment of
               service: "{{organization.helperServices.route.url}}"
             }
           }
+        },
+        {
+          icon: "{{portalBaseUrl}}/anothername.png",
+          config: {
+            someProperty: {
+              someHttpUrl: "{{f74d7d7630da4fa7b961921489c7d3ef.layer2.url}}"
+            }
+          }
         }
       ];
+      const portalUrl: string = "https://fake.maps.arcgis.com";
+      const widgetPath: string = "data.widgetPool.widgets";
+      const layer1: any = {
+        serviceItemId: "2ea59a64b34646f8972a71c7d536e4a3",
+        id: 1
+      };
+      const layer2: any = {
+        serviceItemId: "f74d7d7630da4fa7b961921489c7d3ef",
+        id: 2
+      };
+
+      fetchMock
+        .post("https://fake.com/arcgis/rest/info", {})
+        .post(
+          "https://fake.com/arcgis/rest/services/test/FeatureServer/1",
+          layer1
+        )
+        .post(
+          "https://fake.com/arcgis/rest/services/test/FeatureServer/2",
+          layer2
+        )
+        .post("http://fake.com/arcgis/rest/info", {})
+        .post(
+          "http://fake.com/arcgis/rest/services/test/FeatureServer/1",
+          layer1
+        )
+        .post(
+          "http://fake.com/arcgis/rest/services/test/FeatureServer/2",
+          layer2
+        );
+
+      webmappingapplication
+        .templatizeWidgets(
+          itemTemplate,
+          MOCK_USER_SESSION,
+          portalUrl,
+          widgetPath
+        )
+        .then(
+          (updatedItemTemplate: common.IItemTemplate) => {
+            expect(updatedItemTemplate).toEqual(expectedItemTemplate);
+            done();
+          },
+          e => done.fail()
+        );
+    });
+
+    it("handles widgets that are missing properties", done => {
+      const itemTemplate: common.IItemTemplate = {
+        itemId: "f3223bda3c304dd0bf46dee75ac31aae",
+        type: "Web Mapping Application",
+        key: "abcdefgh",
+        item: {
+          id: "{{f3223bda3c304dd0bf46dee75ac31aae.itemId}}",
+          type: "Web Mapping Application",
+          extent: "{{solutionItemExtent}}",
+          title: "Voting Centers",
+          url:
+            "{{portalBaseUrl}}/apps/webappviewer/index.html?id={{f3223bda3c304dd0bf46dee75ac31aae.itemId}}"
+        },
+        data: {
+          appItemId: "{{f3223bda3c304dd0bf46dee75ac31aae.itemId}}",
+          values: {
+            webmap: "{{myMapId.itemId}}"
+          },
+          map: {
+            appProxy: {
+              mapItemId: "{{mapItemId.itemId}}"
+            },
+            itemId: "{{mapItemId.itemId}}"
+          },
+          dataSource: {
+            dataSources: {},
+            settings: {}
+          },
+          widgetPool: {
+            widgets: [
+              {
+                icon: null,
+                config: null
+              }
+            ]
+          }
+        },
+        resources: [],
+        dependencies: ["myMapId"],
+        circularDependencies: [],
+        properties: {},
+        estimatedDeploymentCostFactor: 2
+      };
+      const expectedItemTemplate: common.IItemTemplate = common.cloneObject(
+        itemTemplate
+      );
       const portalUrl: string = "https://fake.maps.arcgis.com";
       const widgetPath: string = "data.widgetPool.widgets";
       const layer1: any = {
