825fb60c1402064f22b5f27b274cf1d11ee154ca
Mike Tschudi <mtschudi@esri.com>
2020-04-19 10:24:50 -0700

    Boosted Karma adequate coverage minima

--- karma.conf.js
+++ karma.conf.js
@@ -21,10 +21,10 @@ module.exports = function(config) {
       coverageOptions: {
         threshold: {
           global: {
-            statements: 95,
-            branches: 95,
-            functions: 95,
-            lines: 95,
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100,
             excludes: [
               'packages/*/examples/**/*.ts',
               'packages/*/test/**/*.ts'
