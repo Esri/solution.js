9fb8c2bff8401a2b6fe1bde01a9ebf6d7f70adfe
Dave Bouwman <dave@davebouwman.com>
2020-06-11 10:58:37 -0600

    fix typo

--- packages/hub-types/src/helpers/create-hub-request-options.ts
+++ packages/hub-types/src/helpers/create-hub-request-options.ts
@@ -65,7 +65,7 @@ export function createHubRequestOptions(
 
     return {
       authentication,
-      pSelf,
+      portalSelf: pSelf,
       isPortal: pSelf.isPortal,
       hubApiUrl
     } as IHubRequestOptions;
