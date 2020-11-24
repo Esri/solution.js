f8004676a36b37538465f6995201a2787012a793
Mike Tschudi <mtschudi@esri.com>
2020-11-24 10:55:47 -0800

    Fixed portal handling

--- demos/deploySolution/data/appConfig.js
+++ demos/deploySolution/data/appConfig.js
@@ -1,4 +1,13 @@
 const appConfig = {
-  "appId": "byNKUsNNTCNDMxLi",
-  "solutionsGroupId" : "966b251241bb4e3bb9ee3f2b92921aa5"
+  "sources": [{
+    "portal": "https://www.arcgis.com",
+    "solutionsGroupId": "966b251241bb4e3bb9ee3f2b92921aa5"
+  }],
+  "destinations": [{
+    "portal": "https://www.arcgis.com",
+    "appId": "byNKUsNNTCNDMxLi"
+  }, {
+    "portal": "https://rpubs19001.ags.esri.com/portal",
+    "appId": "Q4q8t9mVt4p5Ntfq"
+  }]
 }
