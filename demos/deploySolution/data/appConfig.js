41f09c9ac705fbf8d0ef8bd843a625f70b10159e
Mike Tschudi <mtschudi@esri.com>
2020-11-25 15:05:08 -0800

    Variation selecting portal in advance

--- demos/deploySolution/data/appConfig.js
+++ demos/deploySolution/data/appConfig.js
@@ -1,13 +1,4 @@
 const appConfig = {
-  "sources": [{
-    "portal": "https://www.arcgis.com",
-    "solutionsGroupId": "966b251241bb4e3bb9ee3f2b92921aa5"
-  }],
-  "destinations": [{
-    "portal": "https://www.arcgis.com",
-    "appId": "byNKUsNNTCNDMxLi"
-  }, {
-    "portal": "https://rpubs19001.ags.esri.com/portal",
-    "appId": "Q4q8t9mVt4p5Ntfq"
-  }]
+  "appId": "byNKUsNNTCNDMxLi",
+  "solutionsGroupId" : "966b251241bb4e3bb9ee3f2b92921aa5"
 }
