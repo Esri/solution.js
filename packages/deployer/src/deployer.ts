d1c4c4ac4e8b84fb799eca0ce77e0fc2e819654c
Mike Tschudi <mtschudi@esri.com>
2021-05-03 12:19:31 -0700

    Updated doc

--- packages/deployer/src/deployer.ts
+++ packages/deployer/src/deployer.ts
@@ -39,6 +39,7 @@ import { IModel, isGuid } from "@esri/hub-common";
  * @param maybeModel Item Id or IModel
  * @param authentication Credentials for the destination organization
  * @param options Options to override deployed information and to provide additional credentials
+ * @return The id of the created deployed solution item
  */
 export function deploySolution(
   maybeModel: string | IModel,
