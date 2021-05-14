79e0c4c76f1939d3dad50c8743a82c65b7a8df92
Mike Tschudi <mtschudi@esri.com>
2021-05-14 11:41:28 -0700

    Removed redundant function

--- packages/common/src/resourceHelpers.ts
+++ packages/common/src/resourceHelpers.ts
@@ -72,6 +72,7 @@ import {
   updateItemResource
 } from "@esri/arcgis-rest-portal";
 import { convertItemResourceToStorageResource } from "./resources/convert-item-resource-to-storage-resource";
+import { convertStorageResourceToItemResource } from "./resources/convert-storage-resource-to-item-resource";
 
 import { copyResource } from "./resources/copy-resource";
 import { getBlob } from "./resources/get-blob";
@@ -402,7 +403,7 @@ export function copyMetadata(
  *
  * @param itemId Id of item
  * @return Folder and filename for storage; folder is the itemID suffixed with "_info_metadata"
- * @see generateResourceFilenameFromStorage
+ * @see convertStorageResourceToItemResource
  */
 export function generateMetadataStorageFilename(
   itemId: string
@@ -414,52 +415,6 @@ export function generateMetadataStorageFilename(
     folder: itemId + "_info_metadata",
     filename: "metadata.xml"
   };
-}
-
-/**
- * Extracts an item's resource folder and filename from the filename used to store a copy in a storage item.
- *
- * @param storageResourceFilename Filename used to store the resource, metadata, or thumbnail of an item
- * @return Folder and filename for storing information in an item, as well as the type (resource, metadata,
- * or thumbnail) of the information; the folder property is only meaningful for the resource type
- * @see convertItemResourceToStorageResource
- * @see generateMetadataStorageFilename
- * @see generateThumbnailStorageFilename
- */
-export function generateResourceFilenameFromStorage(
-  storageResourceFilename: string
-): IDeployFilename {
-  let type = EFileType.Resource;
-  // Older Hub Solution Templates don't have folders, so
-  // we have some extra logic to handle this
-  let folder = "";
-  let filename = storageResourceFilename;
-  if (storageResourceFilename.indexOf("/") > -1) {
-    [folder, filename] = storageResourceFilename.split("/");
-  }
-  // let [folder, filename] = storageResourceFilename.split("/");
-
-  // Handle special "folders"
-  if (folder.endsWith("_info_thumbnail")) {
-    type = EFileType.Thumbnail;
-  } else if (folder.endsWith("_info_metadata")) {
-    type = EFileType.Metadata;
-    filename = "metadata.xml";
-  } else if (folder.endsWith("_info_data")) {
-    type = EFileType.Data;
-  } else if (folder.endsWith("_info_dataz")) {
-    filename = filename.replace(/\.zip$/, "");
-    type = EFileType.Data;
-  } else {
-    const folderStart = folder.indexOf("_");
-    if (folderStart > 0) {
-      folder = folder.substr(folderStart + 1);
-    } else {
-      folder = "";
-    }
-  }
-
-  return { type, folder, filename };
 }
 
 /**
@@ -604,7 +559,7 @@ export function generateStorageFilePaths(
         storageItemId,
         resourceFilename
       ),
-      ...generateResourceFilenameFromStorage(resourceFilename)
+      ...convertStorageResourceToItemResource(resourceFilename)
     };
   });
 }
@@ -616,7 +571,7 @@ export function generateStorageFilePaths(
  * @param thumbnailUrlPart Partial path to the thumbnail held in an item's JSON
  * @return Folder and filename for storage; folder is the itemID suffixed with "_info_thumbnail";
  * file is URI-encoded thumbnailUrlPart
- * @see generateResourceFilenameFromStorage
+ * @see convertStorageResourceToItemResource
  */
 export function generateThumbnailStorageFilename(
   itemId: string,
