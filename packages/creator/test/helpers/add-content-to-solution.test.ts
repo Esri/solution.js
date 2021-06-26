cef55eec4a081c4a3dca3d2894205d77ae3b1f4b
Mike Tschudi <mtschudi@esri.com>
2021-06-25 17:40:27 -0700

    Updated tests

--- packages/creator/test/helpers/add-content-to-solution.test.ts
+++ packages/creator/test/helpers/add-content-to-solution.test.ts
@@ -160,11 +160,12 @@ describe("_getDependencies", () => {
       "https://experience.arcgis.com/experience/{{fcb2bf2837a6404ebb418a1f805f976a.itemId}}<div>{{portalBaseUrl}}/apps/webappviewer/index.html?id={{cefb7d787b8b4edb971efba758ee0c1e.itemId}}</div>{{" +
       template.itemId +
       "}}";
+    template.data.operationalLayers[0].title =
+      "ROW {{5f8c7627614a4c179ea5b243509d5b7c}} {{124f66175750431096575c449b42bd65}} {{854f1128cb784cf692e848390452d100}} Permits";
     template.dependencies = ["124f66175750431096575c449b42bd65"];
     expect(_getDependencies(template)).toEqual([
       "124f66175750431096575c449b42bd65",
-      "cefb7d787b8b4edb971efba758ee0c1e",
-      "fcb2bf2837a6404ebb418a1f805f976a"
+      "5f8c7627614a4c179ea5b243509d5b7c"
     ]);
   });
 });
