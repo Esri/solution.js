da4707b1543eca679b224f023ba3caae2b2bba3f
John Hauck <jhauck@esri.com>
2021-08-17 09:41:32 -0600

    add velocityHelpers to common

---
+++ packages/common/src/velocityHelpers.ts
@@ -0,0 +1,107 @@
+/** @license
+ * Copyright 2021 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { UserSession } from "./interfaces";
+import {
+  getSubscriptionInfo,
+  ISubscriptionInfo
+} from "./get-subscription-info";
+import { getProp } from "./generalHelpers";
+
+/**
+ * Get the base velocity url from the current orgs subscription info
+ *
+ * This function will update the input templateDictionary arg with the velocity url
+ * so we can reuse it without pinging the org again for subsequent requests to the
+ * velocity api.
+ *
+ * @param authentication Credentials for the requests
+ * @param templateDictionary Hash of facts: folder id, org URL, adlib replacements
+ *
+ * @return a promise that will resolve with the velocity url
+ *
+ */
+export function getVelocityUrlBase(
+  authentication: UserSession,
+  templateDictionary: any
+): Promise<string> {
+  // if we already have the base url no need to make any additional requests
+  if (templateDictionary.velocityUrl) {
+    return Promise.resolve(templateDictionary.velocityUrl);
+  } else {
+    // get the url from the orgs subscription info
+    return getSubscriptionInfo({ authentication }).then(
+      (subscriptionInfo: ISubscriptionInfo) => {
+        let velocityUrl = "";
+        const orgCapabilities = getProp(subscriptionInfo, "orgCapabilities");
+        /* istanbul ignore else */
+        if (Array.isArray(orgCapabilities)) {
+          orgCapabilities.some(c => {
+            velocityUrl = c.velocityUrl;
+            return velocityUrl;
+          });
+        }
+        // add the base url to the templateDictionary for reuse
+        templateDictionary.velocityUrl = velocityUrl;
+
+        return Promise.resolve(velocityUrl);
+      }
+    );
+  }
+}
+
+export function updateVelocityReferences(
+  data: any,
+  type: string,
+  templateDictionary: any
+): any {
+  if (type === "Web Map") {
+    data?.operationalLayers.forEach((l: any) => {
+      if (
+        l.url &&
+        l.url.indexOf(templateDictionary.velocityUrl) > -1 &&
+        l.itemId
+      ) {
+        delete l.itemId;
+      }
+    });
+  }
+  return _replaceVelocityUrls(data, templateDictionary.velocityUrl);
+}
+
+export function _replaceVelocityUrls(data: any, velocityUrl: string): any {
+  let dataString: string = JSON.stringify(data);
+  if (dataString.indexOf(velocityUrl) > -1) {
+    // replace any instance of the velocity base url
+    dataString = dataString.replace(
+      new RegExp(`${velocityUrl}`, "gi"),
+      "{{velocityUrl}}"
+    );
+
+    // add solutionItemId to any velocity service names
+    const regex = new RegExp("{{velocityUrl}}.+?(?=/[A-Za-z]+Server)", "gi");
+    const results = regex.exec(dataString);
+    if (results) {
+      results.forEach(result => {
+        dataString = dataString.replace(
+          new RegExp(result, "g"),
+          `${result}_{{solutionItemId}}`
+        );
+      });
+    }
+  }
+  return JSON.parse(dataString);
+}
