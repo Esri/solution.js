c4b4ec683364dc683fb0d129ce4899170ab7b07c
Mike Tschudi <mtschudi@esri.com>
2022-02-10 15:31:40 -0800

    Updated deploySolution demo with additional Solutions group

--- demos/deploySolution/data/appConfig.js
+++ demos/deploySolution/data/appConfig.js
@@ -1,3 +1,4 @@
 const appConfig = {
-    "solutionsGroupId" : "966b251241bb4e3bb9ee3f2b92921aa5"
+  "primarySolutionsGroupId" : "966b251241bb4e3bb9ee3f2b92921aa5",
+  "agoBasedEnterpriseSolutionsGroupId": "133b192ad9354e428ec650c01e0df5ad"
 }
