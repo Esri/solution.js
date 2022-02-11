3f96b7e970bcf06d92685e556a7c36f8bef81eb5
Mike Tschudi <mtschudi@esri.com>
2022-02-11 09:50:02 -0800

    Only query the alternate group for the deploySolution demo

--- demos/deploySolution/data/appConfig.js
+++ demos/deploySolution/data/appConfig.js
@@ -1,4 +1,4 @@
 const appConfig = {
-  "primarySolutionsGroupId" : "966b251241bb4e3bb9ee3f2b92921aa5",
+  "primarySolutionsGroupId" : "",
   "agoBasedEnterpriseSolutionsGroupId": "133b192ad9354e428ec650c01e0df5ad"
 }
