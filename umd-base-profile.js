fd91ed5971a64db34b6213b535969e3b48c46f81
Mike Tschudi <mtschudi@esri.com>
2022-02-28 14:31:14 -0800

    Compile umd-*-profile files

--- umd-base-profile.js
+++ umd-base-profile.js
@@ -1,126 +1,97 @@
-import typescript from "rollup-plugin-typescript2";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
-import globalsPlugin from "rollup-plugin-node-globals";
-import builtins from "rollup-plugin-node-builtins";
-
-const path = require("path");
-const fs = require("fs");
-const _ = require("lodash");
-
+"use strict";
+exports.__esModule = true;
+var rollup_plugin_typescript2_1 = require("rollup-plugin-typescript2");
+var plugin_node_resolve_1 = require("@rollup/plugin-node-resolve");
+var plugin_commonjs_1 = require("@rollup/plugin-commonjs");
+var plugin_json_1 = require("@rollup/plugin-json");
+var rollup_plugin_node_globals_1 = require("rollup-plugin-node-globals");
+var rollup_plugin_node_builtins_1 = require("rollup-plugin-node-builtins");
+var path = require("path");
+var fs = require("fs");
+var _ = require("lodash");
 /**
  * Since Rollup runs inside each package we can just get the current
  * package we are bundling.
  */
-const pkg = require(path.join(process.cwd(), "package.json"));
-
+var pkg = require(path.join(process.cwd(), "package.json"));
 /**
  * and dig out its name.
  */
-const { name } = pkg;
-
+var name = pkg.name;
 /**
  * to construct a copyright banner
  */
-
-const copyright = `/* @preserve
-* ${pkg.name} - v${pkg.version} - ${pkg.license}
-* Copyright (c) 2018-${new Date().getFullYear()} Esri, Inc.
-* ${new Date().toString()}
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/`;
-
-
+var copyright = "/* @preserve\n* ".concat(pkg.name, " - v").concat(pkg.version, " - ").concat(pkg.license, "\n* Copyright (c) 2018-").concat(new Date().getFullYear(), " Esri, Inc.\n* ").concat(new Date().toString(), "\n*\n* Licensed under the Apache License, Version 2.0 (the \"License\");\n* you may not use this file except in compliance with the License.\n* You may obtain a copy of the License at\n*\n*    http://www.apache.org/licenses/LICENSE-2.0\n*\n* Unless required by applicable law or agreed to in writing, software\n* distributed under the License is distributed on an \"AS IS\" BASIS,\n* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n* See the License for the specific language governing permissions and\n* limitations under the License.\n*/");
 /**
  * The module name will be the name of the global variable used in UMD builds.
  * All exported members of each package will be attached to this global.
  */
-const moduleName = "arcgisSolution";
-const arcgisRestModuleName = 'arcgisRest'
-const hubModuleName = 'arcgisHub'
-
+var moduleName = "arcgisSolution";
+var arcgisRestModuleName = 'arcgisRest';
+var hubModuleName = 'arcgisHub';
 /**
  * Now we need to discover all the `@esri/solution-*` package names so we can create
  * the `globals` and `externals` to pass to Rollup.
  */
-const packageNames = fs
-  .readdirSync(path.join(__dirname, "packages"))
-  .filter(p => p[0] !== ".")
-  .map(p => {
+var packageNames = fs
+    .readdirSync(path.join(__dirname, "packages"))
+    .filter(function (p) { return p[0] !== "."; })
+    .map(function (p) {
     return require(path.join(__dirname, "packages", p, "package.json")).name;
-  }, {});
-
+}, {});
 /**
  * Now we need to discover all the `@esri/arcgis-rest-*` package names so we can create
  * the `globals` and `externals` to pass to Rollup.
  */
-const peerDependencies = pkg.peerDependencies || [];
-
-const arcgisRestJsPackageNames = Object.keys(peerDependencies)
-  .filter(key => /@esri\/arcgis-rest/.test(key));
-
-const hubJsPackageNames = Object.keys(peerDependencies)
-  .filter(key => /@esri\/hub-/.test(key));
-
+var peerDependencies = pkg.peerDependencies || [];
+var arcgisRestJsPackageNames = Object.keys(peerDependencies)
+    .filter(function (key) { return /@esri\/arcgis-rest/.test(key); });
+var hubJsPackageNames = Object.keys(peerDependencies)
+    .filter(function (key) { return /@esri\/hub-/.test(key); });
 /**
  * Rollup will use this map to determine where to lookup modules on the global
  * window object when neither AMD or CommonJS is being used. This configuration
  * will cause Rollup to lookup all imports from our packages on a single global
  * `arcgisSolution` object.
  */
-const globals = packageNames.reduce((globals, p) => {
-  globals[p] = moduleName;
-  return globals;
+var globals = packageNames.reduce(function (globals, p) {
+    globals[p] = moduleName;
+    return globals;
 }, {});
-
 /**
 * now we tell Rollup to lookup all imports from arcgis-rest-js on a single global
 * `arcgisRest` object.
 */
-arcgisRestJsPackageNames.reduce((globals, p) => {
-  globals[p] = arcgisRestModuleName;
-  return globals;
+arcgisRestJsPackageNames.reduce(function (globals, p) {
+    globals[p] = arcgisRestModuleName;
+    return globals;
 }, globals);
-
-hubJsPackageNames.reduce((globals, p) => {
-  globals[p] = hubModuleName;
-  return globals;
+hubJsPackageNames.reduce(function (globals, p) {
+    globals[p] = hubModuleName;
+    return globals;
 }, globals);
-
 /**
  * Now we can export the Rollup config!
  */
-export default {
-  input: "./src/index.ts",
-  output: {
-    file: `./dist/umd/${name.replace("@esri/solution-", "")}.umd.js`,
-    sourcemap: true,
-    banner: copyright,
-    format: "umd",
-    name: moduleName,
-    globals,
-    extend: true // causes this module to extend the global specified by `moduleName`
-  },
-  context: "window",
-  external: packageNames.concat(arcgisRestJsPackageNames, hubJsPackageNames),
-  plugins: [
-    typescript(),
-    resolve({ preferBuiltins: true, browser: true }),
-    commonjs(),
-    json(),
-    globalsPlugin(),
-    builtins()
-  ]
+exports["default"] = {
+    input: "./src/index.ts",
+    output: {
+        file: "./dist/umd/".concat(name.replace("@esri/solution-", ""), ".umd.js"),
+        sourcemap: true,
+        banner: copyright,
+        format: "umd",
+        name: moduleName,
+        globals: globals,
+        extend: true // causes this module to extend the global specified by `moduleName`
+    },
+    context: "window",
+    external: packageNames.concat(arcgisRestJsPackageNames, hubJsPackageNames),
+    plugins: [
+        (0, rollup_plugin_typescript2_1["default"])(),
+        (0, plugin_node_resolve_1["default"])({ preferBuiltins: true, browser: true }),
+        (0, plugin_commonjs_1["default"])(),
+        (0, plugin_json_1["default"])(),
+        (0, rollup_plugin_node_globals_1["default"])(),
+        (0, rollup_plugin_node_builtins_1["default"])()
+    ]
 };
--- umd-production-profile.js
+++ umd-production-profile.js
@@ -1,13 +1,12 @@
-import config from './umd-base-profile.js';
-import { terser } from "rollup-plugin-terser";
-import filesize from "rollup-plugin-filesize";
-
+"use strict";
+exports.__esModule = true;
+var umd_base_profile_js_1 = require("./umd-base-profile.js");
+var rollup_plugin_terser_1 = require("rollup-plugin-terser");
+var rollup_plugin_filesize_1 = require("rollup-plugin-filesize");
 // use umd.min.js
-config.output.file = config.output.file.replace(".umd.", ".umd.min.");
-
-config.plugins.push(filesize())
-config.plugins.push(terser({
-  output: { comments: /@preserve/ }
-}))
-
-export default config;
+umd_base_profile_js_1["default"].output.file = umd_base_profile_js_1["default"].output.file.replace(".umd.", ".umd.min.");
+umd_base_profile_js_1["default"].plugins.push((0, rollup_plugin_filesize_1["default"])());
+umd_base_profile_js_1["default"].plugins.push((0, rollup_plugin_terser_1.terser)({
+    output: { comments: /@preserve/ }
+}));
+exports["default"] = umd_base_profile_js_1["default"];
