930b12a1b2b7f85c0281ea81858a0515600dda60
Mike Tschudi <mtschudi@esri.com>
2022-03-29 14:27:12 -0700

    Removed non-existent test reporter

--- karma.conf.js
+++ karma.conf.js
@@ -74,8 +74,8 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    // reporters: ["spec", "karma-typescript", "coverage"],
-    reporters: ["dots", "karma-typescript", "coverage"],
+    // reporters: ["spec", "coverage"],
+    reporters: ["dots", "coverage"],
     coverageReporter: {
       // specify a common output directory
       dir: 'coverage',
