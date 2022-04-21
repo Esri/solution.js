046c6ecb6cbc56f5ced7f2c371ac4c19979aacad
Mike Tschudi <mtschudi@esri.com>
2022-04-21 15:30:49 -0700

    Updated packages

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ rem Builds the repository and its demos
 
 call volta install node@16.14.0
 call volta install npm@bundled
-call volta install typescript@4.5.5
+call volta install typescript@4.6.3
 call node --version
 call npm --version
 
