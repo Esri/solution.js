839053e6d0c58f0e67539c7d9a1888dae50f6054
John Hauck <jhauck@esri.com>
2022-09-12 15:34:48 -0600

    update the build batch file

--- build.bat
+++ build.bat
@@ -3,11 +3,12 @@ rem Builds the repository and its demos
 call node --version
 call npm --version
 
-call npm install
-
 pushd demos
+call npm install
 call npm run build
 popd
+
+call npm install
 
 call rmdir/q/s coverage
 call npm run test:chrome:ci
