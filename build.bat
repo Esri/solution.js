8994c7602590bf29fafa5aa6089b1d1a6e71688c
Mike Tschudi <mtschudi@esri.com>
2023-03-31 17:56:13 -0700

    Override npm checks so that install will work

--- build.bat
+++ build.bat
@@ -3,15 +3,15 @@ rem Builds the repository and its demos
 call node --version
 call npm --version
 
-call npm install
+call npm install --force
 
 pushd demos\compareJSON
-call npm install
+call npm install --force
 call npm run build
 popd
 
 pushd demos\getItemInfo
-call npm install
+call npm install --force
 call npm run build
 popd
 
