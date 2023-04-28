89ba91fa7e969e2ce00d29a58b46d3969953148f
Mike Tschudi <mtschudi@esri.com>
2023-04-28 13:38:23 -0700

    Add deploySolution demo

---
+++ demos/deploySolution/src/deploy-solution-main.ts
@@ -0,0 +1,256 @@
+/** @license
+ * Copyright 2019 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// @esri/solution-deployer deploySolution example
+
+import * as common from "@esri/solution-common";
+import * as deployer from "@esri/solution-deployer";
+import * as portal from "@esri/arcgis-rest-portal";
+
+import * as getFormattedItemInfo from "./getFormattedItemInfo";
+
+export interface ISolutionInfoCard {
+  id: string;
+  title: string;
+  version: string;
+}
+
+
+export function deploySolutionsInFolder(
+  folderId: string,
+  srcAuthentication: common.UserSession,
+  destAuthentication: common.UserSession,
+  progressCallback: common.ISolutionProgressCallback,
+  enableItemReuse: boolean,
+  customParams: any
+): Promise<string> {
+  const query = new portal.SearchQueryBuilder()
+  .match(folderId).in("ownerfolder").and()
+  .match("Solution").in("type").and()
+  .match("Template").in("typekeywords");
+  return portal.searchItems({
+    q: query,
+    num: 100,
+    authentication: srcAuthentication
+  })
+  .then((queryResponse) => {
+    if (queryResponse.results.length > 0) {
+      const solutionsToDeploy = queryResponse.results.map(
+        result => {
+          return {
+            id: result.id,
+            title: result.title
+          } as ISolutionInfoCard;
+        }
+      );
+      return deployBatchOfSolutions(solutionsToDeploy, solutionsToDeploy.length, srcAuthentication, destAuthentication, progressCallback, enableItemReuse, customParams);
+    } else {
+      return Promise.resolve("<i>No solutions found in folder</i>");
+    }
+  });
+}
+
+function deployBatchOfSolutions(
+  solutionsToDeploy: ISolutionInfoCard[],
+  totalNumberOfSolutions: number,
+  srcAuthentication: common.UserSession,
+  destAuthentication: common.UserSession,
+  progressCallback: common.ISolutionProgressCallback,
+  enableItemReuse: boolean,
+  customParams: any
+): Promise<string> {
+  // Deploy the first item in the list
+  let solution: ISolutionInfoCard = null;
+  let solutionHtml = "";
+  let deployPromise = Promise.resolve("");
+  if (solutionsToDeploy.length > 0) {
+    solution = solutionsToDeploy.shift();
+    const index = totalNumberOfSolutions - solutionsToDeploy.length;
+    deployPromise = deploySolution(
+      solution,
+      index + '/' + totalNumberOfSolutions + ' "' + solution.title + '"',
+      srcAuthentication,
+      destAuthentication,
+      progressCallback,
+      enableItemReuse,
+      customParams
+    );
+  }
+  return deployPromise
+  .then(html => {
+    solutionHtml = html;
+
+    // Are there any more items in the list?
+    let remainingDeployPromise = Promise.resolve("");
+    if (solutionsToDeploy.length > 0) {
+      remainingDeployPromise = deployBatchOfSolutions(solutionsToDeploy, totalNumberOfSolutions,
+        srcAuthentication, destAuthentication, progressCallback, enableItemReuse, customParams);
+    }
+    return remainingDeployPromise;
+  })
+  .then(remainingHtml => {
+    return solutionHtml + remainingHtml;
+  });
+}
+
+export function deploySolution(
+  templateSolution: ISolutionInfoCard,
+  jobId: string,
+  srcAuthentication: common.UserSession,
+  destAuthentication: common.UserSession,
+  progressCallback: common.ISolutionProgressCallback,
+  enableItemReuse: boolean,
+  customParams: any
+): Promise<string> {
+  // Deploy a solution described by the supplied id
+  const options: common.IDeploySolutionOptions = {
+    jobId,
+    progressCallback: progressCallback,
+    consoleProgress: true,
+    storageAuthentication: srcAuthentication,
+    enableItemReuse,
+    templateDictionary: isJsonStr(customParams) ? {
+      params: JSON.parse(customParams)
+    } : {}
+  };
+  const itemUrlPrefix = destAuthentication.portal.replace("/sharing/rest", "");
+
+  return deployer.deploySolution(templateSolution.id, destAuthentication, options)
+  .then((deployedSolution: any) => {
+    return 'Deployed solution "' + templateSolution.title + '" into <a href="' + itemUrlPrefix +
+      '/home/item.html?id=' + deployedSolution + '" target="_blank">' + deployedSolution + '</a><br/>';
+  });
+}
+
+export function deployAndDisplaySolution(
+  templateSolutionId: string,
+  srcAuthentication: common.UserSession,
+  destAuthentication: common.UserSession,
+  progressCallback: common.ISolutionProgressCallback,
+  enableItemReuse: boolean,
+  customParams: any
+): Promise<string> {
+  // Deploy a solution described by the supplied id
+  // only pass custom params to the templateDictionary
+  const options: common.IDeploySolutionOptions = {
+    jobId: templateSolutionId,
+    progressCallback: progressCallback,
+    consoleProgress: true,
+    storageAuthentication: srcAuthentication,
+    enableItemReuse,
+    templateDictionary: isJsonStr(customParams) ? {
+      params: JSON.parse(customParams)
+    } : {}
+  };
+
+  return deployer.deploySolution(templateSolutionId, destAuthentication, options)
+  .then((deployedSolution: any) => {
+    return getFormattedItemInfo.getFormattedItemInfo(deployedSolution, destAuthentication);
+  });
+}
+
+export function getFolders(
+  authentication: common.UserSession,
+) : Promise<ISolutionInfoCard[]>{
+  return portal.getUserContent({ authentication })
+  .then((response: portal.IUserContentResponse) => {
+    return response.folders.map(
+      (folder: portal.IFolder) => {
+        return {
+          id: folder.id,
+          title: folder.title
+        } as ISolutionInfoCard;
+      }
+    );
+  });
+}
+
+export function getTemplates(
+  primarySolutionsGroupId: string,
+  agoBasedEnterpriseSolutionsGroupId: string,
+  authentication?: common.UserSession
+) : Promise<ISolutionInfoCard[]>{
+  const query = "type: Solution typekeywords:Solution,Template"
+  const anonUS = new common.UserSession({portal:"https://www.arcgis.com/sharing/rest"});
+  const additionalSearchOptions = {
+    sortField: "title",
+    sortOrder: "asc"
+  }
+
+  const searches: Array<Promise<common.ISearchResult<portal.IItem>>> = [];
+  if (primarySolutionsGroupId) {
+    searches.push(common.searchGroupAllContents(
+      primarySolutionsGroupId, query, authentication || anonUS, additionalSearchOptions));
+  }
+  if (agoBasedEnterpriseSolutionsGroupId) {
+    searches.push(common.searchGroupAllContents(
+      agoBasedEnterpriseSolutionsGroupId, query, authentication || anonUS, additionalSearchOptions));
+  }
+
+  return Promise.all(searches)
+  .then(responseSet => {
+    let items: portal.IItem[] = [];
+
+    // Bundle the results
+    if (responseSet.length === 1) {
+      items = responseSet[0].results;
+    } else if (responseSet.length === 2) {
+      items = responseSet[0].results.concat(responseSet[1].results);
+    }
+
+    // Screen the results
+    const cleanResults = common.sanitizeJSON(items);
+
+    // Construct the results
+    const cardList: ISolutionInfoCard[] = [];
+    cleanResults.forEach((result:any) => {
+      // Extract version for this item
+      let version = "";
+      (result.typeKeywords || []).some(
+        (element: string) => {
+          if (element.startsWith("solutionversion-")) {
+            version = element.substring("solutionversion-".length);
+            return true;
+          }
+          return false;
+        }
+      );
+
+      // Add item to results list
+      const card: ISolutionInfoCard = {
+        id: result.id,
+        title: result.title,
+        version
+      }
+      cardList.push(card)
+    });
+
+    // And return them
+    return Promise.resolve(cardList);
+  });
+}
+
+export function isJsonStr(
+  v: string
+): boolean {
+  // string must contain valid json object with at least one key
+  let parsedValue = {};
+  try {
+    parsedValue = JSON.parse(v);
+  } catch (e) {
+    return false;
+  }
+  return Object.keys(parsedValue).length > 0;
+}
---
+++ demos/deploySolution/src/index.ts
@@ -0,0 +1,248 @@
+/** @license
+ * Copyright 2023 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import "./style.css";
+import * as appConfig from "../data/appConfig.json";
+import * as common from "@esri/solution-common";
+import * as htmlUtil from "./htmlUtil";
+import * as main from "./deploy-solution-main";
+
+declare var fetchFoldersFcn: any;
+declare var goFcn: any;
+declare var updateDestAuthFcn: any;
+
+//--------------------------------------------------------------------------------------------------------------------//
+
+function fetchFolders(
+): void {
+  // Source credentials
+  const srcPortal = (htmlUtil.getHTMLValue("srcPortal") || "https://www.arcgis.com") + "/sharing/rest";
+  const srcCreds = new common.UserSession({
+    username: htmlUtil.getHTMLValue("srcUsername"),
+    password: htmlUtil.getHTMLValue("srcPassword"),
+    portal: srcPortal
+  });
+
+  // Populate folders picklist
+  const foldersSelect = document.getElementById("foldersPicklist") as HTMLSelectElement;
+  const numOptions = foldersSelect.options.length - 1;
+  for(let i = numOptions; i >= 0; i--) {
+    foldersSelect.remove(i);
+  }
+  foldersSelect.add(document.createElement("option"));
+
+  main.getFolders(srcCreds)
+  .then(folders => {
+    folders.forEach((folder) => {
+      var option = document.createElement("option");
+      option.value = folder.id;
+      option.text = folder.title + " ( " + folder.id + " )";
+      foldersSelect.add(option);
+    });
+  });
+}
+
+// Deploy!
+function deploySolution(
+  solutionId: string,
+  folderId: string,
+  srcCreds: common.UserSession,
+  destCreds: common.UserSession,
+  useExisting: boolean,
+  customParams: any
+): void {
+  const startTime = Date.now();
+  const createdItems = [] as any[];
+
+  let deployPromise = Promise.resolve("<i>No Solution(s) provided</i>");
+  const progressFcn =
+    function (percentDone, jobId, progressEvent) {
+      if (progressEvent) {
+        createdItems.push(progressEvent.data);
+      }
+      let html = "Deploying " + jobId + "..." + percentDone.toFixed().toString() + "%" + "<br><br>Finished items:<ol>";
+      createdItems.forEach(function (item) { return html += "<li>" + item + "</li>" });
+      html += "</ol>";
+      document.getElementById("output").innerHTML = html;
+    } as common.ISolutionProgressCallback;
+  if (solutionId.length > 0) {
+    deployPromise = main.deployAndDisplaySolution(
+      solutionId,
+      srcCreds,
+      destCreds,
+      progressFcn,
+      useExisting,
+      customParams
+    );
+  } else if (folderId.length > 0) {
+    deployPromise = main.deploySolutionsInFolder(
+      folderId,
+      srcCreds,
+      destCreds,
+      progressFcn,
+      useExisting,
+      customParams
+    );
+  }
+
+  deployPromise.then(function (html){
+      reportElapsedTime(startTime);
+      document.getElementById("output").innerHTML = html;
+    },
+    function (error) {
+      const message = error?.error || JSON.stringify(error) || "Unspecified error";
+      reportElapsedTime(startTime);
+      document.getElementById("output").innerHTML = "<span style=\"color:red\">" + message + "</span>";
+    }
+  );
+}
+
+/**
+ * Runs the solution deployment.
+ */
+function go(
+): void {
+  document.getElementById("input").style.display = "none";
+  document.getElementById("output").style.display = "block";
+
+  // use the manually entered value, falling back to the select lists
+  const solutionId =
+    htmlUtil.getHTMLValue("solutionId") ||
+    htmlUtil.getHTMLValue("solutionPicklist");
+  const folderId = htmlUtil.getHTMLValue("foldersPicklist");
+
+  // Use Existing
+  const useExisting = htmlUtil.getHTMLChecked("useExistingChk");
+
+  // Custom Params
+  const customParams = htmlUtil.getHTMLValue("customParams");
+
+  // Source credentials
+  const srcPortal = (htmlUtil.getHTMLValue("srcPortal") || "https://www.arcgis.com") + "/sharing/rest";
+  const srcCreds = new common.UserSession({
+    username: htmlUtil.getHTMLValue("srcUsername"),
+    password: htmlUtil.getHTMLValue("srcPassword"),
+    portal: srcPortal
+  });
+
+  // Dest credentials
+  const destPortal = (htmlUtil.getHTMLValue("destPortal") || "https://www.arcgis.com") + "/sharing/rest";
+  let destCreds;
+  if (!htmlUtil.getHTMLChecked("destAuthType")) {
+    destCreds = new common.UserSession({
+      username: htmlUtil.getHTMLValue("destUsername"),
+      password: htmlUtil.getHTMLValue("destPassword"),
+      portal: destPortal,
+      clientId: htmlUtil.getHTMLValue("clientId")
+    });
+    deploySolution(solutionId, folderId, srcCreds, destCreds, useExisting, customParams);
+  } else {
+    const redirect_uri = window.location.origin + window.location.pathname;
+    const clientId = htmlUtil.getHTMLValue("clientId");
+    common.UserSession.beginOAuth2({
+      clientId: clientId,
+      redirectUri: redirect_uri + 'authenticate.html?clientID=' + clientId,
+      popup: true,
+    }).then(function (newSession) {
+      // Upon a successful login, update the session with the new session.
+      session = newSession;
+      updateSessionInfo(session);
+      deploySolution(solutionId, folderId, srcCreds, session, useExisting, customParams);
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+}
+
+function updateDestAuth(
+  checkbox: HTMLInputElement
+): void {
+  var destCredentials = document.getElementById("destCredentials");
+  var clientIdParent = document.getElementById("clientIdParent");
+
+  if (checkbox.checked) {
+    clientIdParent.classList.remove("display-none");
+    destCredentials.classList.add("display-none");
+  } else {
+    clientIdParent.classList.add("display-none");
+    destCredentials.classList.remove("display-none");
+  }
+}
+
+// Function to update the UI with session info.
+function updateSessionInfo(
+  session: common.UserSession
+): void {
+  if (session) {
+    localStorage.setItem('__SOLUTION_JS_USER_SESSION__', session.serialize());
+  }
+}
+
+/**
+ * Reports an elapsed time to the console log.
+ *
+ * @param startTime `Date` to use as a start time
+ */
+function reportElapsedTime(
+  startTime: number
+): void {
+  var endTime = Date.now();
+  console.log("Elapsed time: " + ((endTime - startTime) / 1000).toFixed(1) + " seconds");
+}
+
+//--------------------------------------------------------------------------------------------------------------------//
+
+fetchFoldersFcn = fetchFolders;
+goFcn = go;
+updateDestAuthFcn = updateDestAuth;
+
+// Populate solution picklist
+const solutionsSelect = document.getElementById("solutionPicklist") as HTMLSelectElement;
+solutionsSelect.add(document.createElement("option"));
+
+console.log("appConfig", appConfig, appConfig.primarySolutionsGroupId, appConfig.agoBasedEnterpriseSolutionsGroupId);//???
+main.getTemplates(
+  appConfig.primarySolutionsGroupId, appConfig.agoBasedEnterpriseSolutionsGroupId
+).then((solutions) => {
+  solutions.forEach((solution) => {
+    var option = document.createElement("option");
+    option.value = solution.id
+    option.text = solution.title + " ( " + solution.id + " )";
+    solutionsSelect.add(option);
+  });
+});
+
+// Define a global session variable.
+let session: common.UserSession = null;
+
+// Check to see if there is a serialized session in local storage.
+const serializedSession = localStorage.getItem('__SOLUTION_JS_USER_SESSION__');
+if (serializedSession !== null && serializedSession !== "undefined") {
+  // If there is a serialized session, parse it and create a new session object.
+  let parsed = JSON.parse(serializedSession);
+  // Cast the tokenExpires property back into a date.
+  parsed.tokenExpires = new Date(parsed.tokenExpires);
+  // Create the new session object.
+  session = new common.UserSession(parsed);
+
+  // Clear the previous session.
+  localStorage.removeItem('__SOLUTION_JS_USER_SESSION__');
+}
+
+// Call the function on page load to set current state.
+updateSessionInfo(session);
+
+document.getElementById("container").style.display = "block";
