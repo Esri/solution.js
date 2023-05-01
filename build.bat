bad06bb3bbfdd3733629f5ba70440ae358282d67
Mike Tschudi <mtschudi@esri.com>
2023-05-01 14:38:48 -0700

    Symlink demos and packages

--- build.bat
+++ build.bat
@@ -6,37 +6,30 @@ call npm --version
 call npm install
 
 pushd demos\compareJSON
-call npm install
 call npm run build
 popd
 
 pushd demos\copyItemInfo
-call npm install
 call npm run build
 popd
 
 pushd demos\copySolutions
-call npm install
 call npm run build
 popd
 
 pushd demos\createSolution
-call npm install
 call npm run build
 popd
 
 pushd demos\deleteSolution
-call npm install
 call npm run build
 popd
 
 pushd demos\getItemInfo
-call npm install
 call npm run build
 popd
 
 pushd demos\verifySolution
-call npm install
 call npm run build
 popd
 
