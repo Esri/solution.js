702c182f227a7cbb86ef7350f03444e2b77dc3bd
Mike Tschudi <mtschudi@esri.com>
2023-10-12 12:22:08 -0700

    Refactor and add tests

--- packages/group/src/group.ts
+++ packages/group/src/group.ts
@@ -113,34 +113,7 @@ export function createItemFromTemplate(
     newItemTemplate.item.thumbnail = template.item.thumbnail;
 
     // Set up properties needed to create group
-    const newGroup: common.IGroupAdd = {
-      access: "private",
-      description: newItemTemplate.item.description,
-      owner: newItemTemplate.item.owner,
-      snippet: newItemTemplate.item.snippet,
-      tags: newItemTemplate.item.tags,
-      thumbnail: newItemTemplate.item.thumbnail,
-      title: newItemTemplate.item.title || "",
-      typeKeywords: newItemTemplate.item.typeKeywords
-    };
-
-    const props: string[] = [
-      "autoJoin",
-      "displaySettings",
-      "isInvitationOnly",
-      "isOpenData",
-      "isViewOnly",
-      "membershipAccess",
-      "properties",
-      "sortField",
-      "sortOrder"
-    ];
-    props.forEach(p => {
-      /* istanbul ignore else */
-      if (newItemTemplate.item.hasOwnProperty(p)) {
-        newGroup[p] = newItemTemplate.item[p];
-      }
-    })
+    const newGroup = _initializeNewGroup(newItemTemplate.item);
 
     // Create a group, appending a sequential suffix to its name if the group exists, e.g.,
     //  * Manage Right of Way Activities
@@ -392,4 +365,40 @@ export function postProcess(
     }
   });
   return promise;
+}
+
+export function _initializeNewGroup(
+  sourceGroup: common.IItemGeneralized
+): common.IGroupAdd {
+  // Set up properties needed to create group
+  const newGroup: common.IGroupAdd = {
+    access: "private",
+    description: sourceGroup.description,
+    owner: sourceGroup.owner,
+    snippet: sourceGroup.snippet,
+    tags: sourceGroup.tags,
+    thumbnail: sourceGroup.thumbnail,
+    title: sourceGroup.title || "",
+    typeKeywords: sourceGroup.typeKeywords
+  };
+
+  const props: string[] = [
+    "autoJoin",
+    "displaySettings",
+    "isInvitationOnly",
+    "isOpenData",
+    "isViewOnly",
+    "membershipAccess",
+    "properties",
+    "sortField",
+    "sortOrder"
+  ];
+  props.forEach(p => {
+    /* istanbul ignore else */
+    if (sourceGroup.hasOwnProperty(p)) {
+      newGroup[p] = sourceGroup[p];
+    }
+  })
+
+  return newGroup;
 }
