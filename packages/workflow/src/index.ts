95e14cad1a6a549c547e055d148f30c571c6e9ff
Mike Tschudi <mtschudi@esri.com>
2024-02-21 07:18:07 -0800

    snapshot

--- packages/workflow/src/index.ts
+++ packages/workflow/src/index.ts
@@ -21,3 +21,4 @@
  */
 
 export * from "./workflow";
+export * from "./workflowHelpers";
---
+++ packages/workflow/src/workflowHelpers.ts
@@ -0,0 +1,75 @@
+/** @license
+ * Copyright 2024 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Manages the creation and deployment of workflow item types.
+ *
+ * @module workflow
+ */
+
+import * as common from "@esri/solution-common";
+import { IRequestOptions, request } from "@esri/arcgis-rest-request";
+
+// ------------------------------------------------------------------------------------------------------------------ //
+
+export async function addWorkflowItem(
+  item: common.IItemTemplate,
+  //destinationFolderId: string,
+  authentication: common.UserSession
+): Promise<common.IItemTemplate> {
+  const user = await authentication.getUser({ authentication });
+
+  // Add the workflow item
+  const workflowUrl = `https://workflow.arcgis.com/${user.orgId}/admin/createWorkflowItem?name=${item.item.title}`;
+  const requestOptions: IRequestOptions = {
+    authentication: authentication,
+    params: {
+      //async: true,
+      f: "json",
+      token: authentication.token
+    }
+  };
+
+  const createdWorkflowResponse = await request(workflowUrl, requestOptions);
+  console.log("Created workflow item: ", JSON.stringify(createdWorkflowResponse, null, 2));//???
+
+  // Fetch the auxiliary items
+  const workflowItemId = createdWorkflowResponse.itemId;
+  const workflowLocationsItemName = `WorkflowLocations_${workflowItemId}`;
+  const workflowViewsItemName = `workflow_views_${workflowItemId}`;
+  const workflowItemName = `workflow_${workflowItemId}`;
+  console.log("auxiliary items: ", workflowLocationsItemName, workflowViewsItemName, workflowItemName);//???
+
+  const fetchResults = await Promise.all([
+    common.getItemBase(workflowLocationsItemName, authentication),
+    common.getItemBase(workflowViewsItemName, authentication),
+    common.getItemBase(workflowItemName, authentication)
+  ]);
+  console.log("auxiliary items: ", JSON.stringify(fetchResults.map(item => item.id), null, 2));//???
+
+  // Move the workflow and auxiliary items to the destination
+  const
+  const moveResults = await Promise.all([
+    common.moveItem(workflowItemId, item.item.owner, authentication),
+    common.moveItem(fetchResults[0].id, item.item.owner, authentication),
+    common.moveItem(fetchResults[1].id, item.item.owner, authentication),
+    common.moveItem(fetchResults[2].id, item.item.owner, authentication)
+  ]);
+
+
+
+  return Promise.resolve(item);
+}
