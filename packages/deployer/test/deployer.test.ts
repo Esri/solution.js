ae64c5f73a6b4dedd7c96a8607ff5b24e5fc3c9f
Mike Tschudi <mtschudi@esri.com>
2024-04-17 06:26:50 -0700

    Updated deployOptions with information needed by workflow

--- packages/deployer/test/deployer.test.ts
+++ packages/deployer/test/deployer.test.ts
@@ -644,7 +644,8 @@ describe("Module `deployer`", () => {
       const options: common.IDeploySolutionOptions = {
         templateDictionary: templateDictionary,
         progressCallback: testUtils.SOLUTION_PROGRESS_CALLBACK,
-        consoleProgress: true
+        consoleProgress: true,
+        workflowManagerUrl: "https://myorg.com/workflow"
       };
       deployer
         .deploySolution(itemInfo.item.id, MOCK_USER_SESSION, options)
