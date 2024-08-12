d78a9340c507d3a0df2d420bcf8f43785b27c9f2
John Hauck <jhauck@esri.com>
2024-08-12 14:21:50 -0600

    review comments

--- packages/common/src/workflowHelpers.ts
+++ packages/common/src/workflowHelpers.ts
@@ -312,20 +312,22 @@ export function updateWorkflowTemplateIds(
 }
 
 /**
- * Get the various dependencies from the workflow item
+ * Get the various dependencies from the workflow item and add them to the current templates dependency list
  *
  * @param template The IItemTemplate of the workflow item
  *
  */
 export function getWorkflowDependencies(template: interfaces.IItemTemplate): void {
-  const keys = ["viewSchema", "workflowLocations", "workflowSchema"];
-  keys.forEach((k) => {
-    const data = template.data;
-    if (data && Object.keys(data).indexOf(k) > -1) {
-      const id = data[k].itemId;
-      if (id && template.dependencies.indexOf(id) < 0) {
-        template.dependencies.push(id);
+  const data = template.data;
+  if (data) {
+    const keys = ["viewSchema", "workflowLocations", "workflowSchema"];
+    keys.forEach((k) => {
+      if (Object.keys(data).indexOf(k) > -1) {
+        const id = data[k].itemId;
+        if (id && template.dependencies.indexOf(id) < 0) {
+          template.dependencies.push(id);
+        }
       }
-    }
-  });
+    });
+  }
 }
