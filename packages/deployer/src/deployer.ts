62373c229d249648478eb5f5c6d119f8a8271500
Previn Wong <PWong@esri.com>
2025-09-15 10:47:36 -0700

    Update deployer.ts

--- packages/deployer/src/deployer.ts
+++ packages/deployer/src/deployer.ts
@@ -120,16 +120,14 @@ export async function deploySolution(
       },
     )
     .catch((ex) => {
-      deployCatchHandler(ex, authentication);
+      if (typeof ex === "string") {
+        deployCatchHandler(ex, authentication);
+      }
       throw ex;
     });
 }
 
-export function deployCatchHandler(ex: any, authentication: common.UserSession) {
-  if (typeof ex !== "string") {
-    return;
-  }
-
+export function deployCatchHandler(ex: string, authentication: common.UserSession) {
   const options: common.IDeleteSolutionOptions = {
     consoleProgress: true,
     sendToRecycling: false,
